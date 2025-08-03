b6e3b7c95bdd6304b0576dedb02a1093a57279bb
Erik <w.erik.galardi@gmail.com>
2025-08-02 22:54:40 -0400

    Deploy: Set TrustDashboard as default landing view

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TrustDashboard from './TrustDashboard';
-import Home from './Home';
 import './home.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <BrowserRouter basename="/">
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<TrustDashboard />} />
+      <Route path="/" element={<TrustDashboard />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
