9db27fd621314441addf6cf18ba0f4a5c97385ff
Erik <w.erik.galardi@gmail.com>
2025-08-02 22:52:31 -0400

    Final routing fix for live dashboard

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import TrustDashboard from './TrustDashboard';
 import Home from './Home';
 import './home.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/">
+  <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<TrustDashboard />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
